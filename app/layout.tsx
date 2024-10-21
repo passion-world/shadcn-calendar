9ec2422e410c1fbba2491fb9356713b9952b4499
passion-world <andyjin0102@gmail.com>
2024-10-21 11:00:49 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Providers>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           {/* <Navbar /> */}
           <div className="relative flex flex-col h-screen">
             <main className="w-full flex-grow">{children}</main>
